2d94eb02aae8d459d6022c9b8e8fadc7b08a9b84
Mazharul Islam <505mazharulislam@gmail.com>
2023-12-22 22:08:22 +0600

    job feature

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,11 +1,22 @@
 
-const Job = ({job}) => {
+const Job = ({ job }) => {
 
-    const {logo} = job;
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
 
     return (
-        <div>
-            <img src= {logo} alt="" />
+        <div className="card card-compact bg-base-100 shadow-xl">
+            <figure><img src= {logo} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="text-2xl font-semibold">{job_title}</h2>
+                <p>{company_name}</p>
+                <div>
+                    <button className="text-[#7E90FE] px-5 py-2 font-bold border rounded border-[#7E90FE] mr-4">{remote_or_onsite}</button>
+                    <button className="text-[#7E90FE] px-5 py-2 font-bold border rounded border-[#7E90FE] mr-4">{job_type}</button>
+                </div>
+                <div className="card-actions ">
+                    <button className="btn btn-primary">View Details</button>
+                </div>
+            </div>
         </div>
     );
 };
